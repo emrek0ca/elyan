884966eedceb9627a02b03cbdfcb45ad5f3042b9
Emre <osmanemrekoca@gmail.com>
2026-03-21 19:33:57 +0300

    chore: add cloudflare agents package export

---
+++ core/cloudflare_agents/__init__.py
@@ -0,0 +1,17 @@
+from .project import (
+    build_cloudflare_agents_bundle,
+    build_cloudflare_agents_prompt,
+    detect_project_root,
+    resolve_project_root,
+    scaffold_project,
+    summarize_project,
+)
+
+__all__ = [
+    "build_cloudflare_agents_bundle",
+    "build_cloudflare_agents_prompt",
+    "detect_project_root",
+    "resolve_project_root",
+    "scaffold_project",
+    "summarize_project",
+]
